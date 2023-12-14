4eb94c2a21371827bc2805dcf3e53fb4487165c9
vaishnv-sn <vaishnav.sn4@gmail.com>
2023-12-14 19:19:10 +0530

    feat: Implement displayPDF endpoint for retrieving and displaying PDF file

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -1,18 +1,40 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 
 module.exports = {
     uploadPDF: (req, res) => {
         try {
             const fileId = req.fileId;
             if (fileId) {
-                res.status(200).json(fileId);
+                return res.status(200).json(fileId);
             }
         } catch (error) {
             console.log(error);
             res.status(500).json({ error: 'Server Unexpected Error, Please try again later!' });
         }
     },
-    displayPDF: (req, res) => {
-        console.log('display-api-true');
+    displayPDF: async (req, res) => {
+        try {
+            const { fileId } = req.query;
+            const filePath = path.join(__dirname, '..', 'uploads', `${fileId}.pdf`);
+
+            const fileExists = await fs.promises.access(filePath, fs.constants.F_OK)
+                .then(() => true)
+                .catch(() => false);
+
+            if (!fileExists) {
+                return res.status(404).json({ error: 'File not found, try again.' });
+            };
+
+            const fileData = await fs.promises.readFile(filePath);
+
+            return res.status(200).setHeader('Content-Type', 'application/pdf').send(fileData);
+        } catch (error) {
+            console.error('Error:', err);
+            res.status(500).json({ error: 'Server Unexpected Error, Please try again later!' });
+        }
     },
     createNewPDF: (req, res) => {
         console.log(req.body);
